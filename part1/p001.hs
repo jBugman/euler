6b4f9cdaedae68b911fbf7352e82005c52948bf8
Sergey Parshukov <codedby@bugman.me>
2015-11-26 18:21:45 +0300

    Some linting

--- part1/p001.hs
+++ part1/p001.hs
@@ -1,19 +1,11 @@
 module Main where
--- Generalized Project Euler problem 1 solution
 
 main :: IO ()
-main = mapM_ printline [1..9]
+main = print $ solution 1000 3 5
 
-printline :: Integer -> IO ()
-printline n = do
-  let x = 10 ^ n
-  putStr . show $ x
-  putStr " -> "
-  print $ result x 3 5
-
-result :: Integer -> Integer -> Integer -> Integer
-result x y z = sum factors where
-  factors = filter factor35 numbers
-  factor35 x = (isFactor y x) || (isFactor z x)
-  isFactor x y = (mod y x) == 0
+solution :: Integer -> Integer -> Integer -> Integer
+solution x y z = sum factors where
+  factors = filter factorYZ numbers
+  factorYZ n = isFactor y n || isFactor z n
+  isFactor n m = mod m n == 0
   numbers = [1..(x - 1)]
